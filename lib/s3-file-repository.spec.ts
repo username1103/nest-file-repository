82f6be4f9414b60a4d51af9e018a723ff922b9f6
Kim Myeongil <mikim961103@gmail.com>
2023-05-20 01:06:42 +0900

    feat: add s3-file-repository

---
+++ lib/s3-file-repository.spec.ts
@@ -0,0 +1,91 @@
+import { UploadStrategy } from './enum/upload-strategy';
+import { NoSuchBucketException } from './exception/no-such-bucket.exception';
+import { TimeoutException } from './exception/timeout.exception';
+import { File } from './File';
+import { S3FileUploadConfiguration } from './interface/file-upload-configuration';
+import { S3FileRepository } from './s3-file-repository';
+
+describe('S3FileRepository', () => {
+  it('upload file in s3', async () => {
+    // given
+    const confg: S3FileUploadConfiguration = {
+      strategy: UploadStrategy.S3,
+      options: {
+        path: 'test-path',
+        region: 'test',
+        credentials: {
+          accessKeyId: 'test',
+          secretAccessKey: 'test',
+        },
+        endPoint: 'http://localhost:4566',
+        forcePathStyle: true,
+        bucket: 'test-bucket',
+        acl: 'public-read',
+      },
+    };
+    const s3FileRepository = new S3FileRepository(confg);
+
+    const file = new File('file.txt', Buffer.from('hello'));
+
+    // when
+    const response = await s3FileRepository.save(file);
+
+    // then
+    expect(response).toBe('test-path/file.txt');
+  });
+
+  it('throw timeout exception when it times out', async () => {
+    // given
+    const config: S3FileUploadConfiguration = {
+      strategy: UploadStrategy.S3,
+      options: {
+        path: 'test-path',
+        region: 'test',
+        credentials: {
+          accessKeyId: 'test',
+          secretAccessKey: 'test',
+        },
+        endPoint: 'http://localhost:4566',
+        forcePathStyle: true,
+        bucket: 'test-bucket',
+        acl: 'public-read',
+        timeout: 1,
+      },
+    };
+    const s3FileRepository = new S3FileRepository(config);
+
+    const file = new File('file.jpeg', Buffer.from('hello'));
+
+    // when, then
+    await expect(() => s3FileRepository.save(file)).rejects.toThrow(
+      TimeoutException,
+    );
+  });
+
+  it('throw NoSuchBucketException when bucket does not exist', async () => {
+    // given
+    const config: S3FileUploadConfiguration = {
+      strategy: UploadStrategy.S3,
+      options: {
+        path: 'test-path',
+        region: 'test',
+        credentials: {
+          accessKeyId: 'test',
+          secretAccessKey: 'test',
+        },
+        endPoint: 'http://localhost:4566',
+        forcePathStyle: true,
+        bucket: 'invalid',
+        acl: 'public-read',
+      },
+    };
+    const s3FileRepository = new S3FileRepository(config);
+
+    const file = new File('file.jpeg', Buffer.from('hello'));
+
+    // when, then
+    await expect(() => s3FileRepository.save(file)).rejects.toThrow(
+      NoSuchBucketException,
+    );
+  });
+});
--- lib/s3-file-repository.ts
+++ lib/s3-file-repository.ts
@@ -1,5 +1,11 @@
+import path from 'path';
+
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { NodeHttpHandler } from '@aws-sdk/node-http-handler';
 import { Inject } from '@nestjs/common';
 
+import { NoSuchBucketException } from './exception/no-such-bucket.exception';
+import { TimeoutException } from './exception/timeout.exception';
 import { File } from './File';
 import { FileRepository } from './file-repository';
 import {
@@ -8,11 +14,53 @@ import {
 } from './interface/file-upload-configuration';
 
 export class S3FileRepository implements FileRepository {
+  private readonly client: S3Client;
   constructor(
     @Inject(CONFIG) private readonly config: S3FileUploadConfiguration,
-  ) {}
+  ) {
+    this.client = new S3Client({
+      region: this.config.options.region,
+      endpoint: this.config.options.endPoint,
+      forcePathStyle: this.config.options.forcePathStyle,
+      credentials: {
+        accessKeyId: this.config.options.credentials.accessKeyId,
+        secretAccessKey: this.config.options.credentials.secretAccessKey,
+      },
+      requestHandler: new NodeHttpHandler({
+        requestTimeout: this.config.options.timeout,
+      }),
+    });
+  }
 
-  save(file: File): Promise<string> {
-    return Promise.resolve('');
+  async save(file: File): Promise<string> {
+    const filePath = path.join(this.config.options?.path ?? '', file.filename);
+
+    try {
+      await this.client.send(
+        new PutObjectCommand({
+          Bucket: this.config.options.bucket,
+          Key: filePath,
+          Body: file.data,
+          ACL: this.config.options.acl,
+          ContentType: file.mimetype,
+        }),
+      );
+    } catch (e) {
+      if (e.name === 'TimeoutError') {
+        throw new TimeoutException(
+          `raise timeout: ${this.config.options.timeout}ms`,
+        );
+      }
+
+      if (e.name === 'NoSuchBucket') {
+        throw new NoSuchBucketException(
+          `not exist bucket: ${this.config.options.bucket}`,
+        );
+      }
+
+      throw e;
+    }
+
+    return filePath;
   }
 }
