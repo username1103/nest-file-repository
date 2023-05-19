f89b6cea23f2ca34c86ea6096d970f208b977676
Kim Myeongil <mikim961103@gmail.com>
2023-05-20 02:36:01 +0900

    fix: fix file-repository.module.spec.ts test case

--- lib/file-repository.module.spec.ts
+++ lib/file-repository.module.spec.ts
@@ -7,12 +7,14 @@ import { FileRepositoryModule } from './file-repository.module';
 import {
   CONFIG,
   DiskFileUploadConfiguration,
+  S3FileUploadConfiguration,
 } from './interface/file-upload-configuration';
-import { NAME_GENERATOR } from './interface/name-generator';
-import { UuidNameGenerator } from './util/uuid-name-generator';
+import { MIMETYPE_EXTENSION_CONVERTER } from './interface/mimetype-extension-converter';
+import { S3FileRepository } from './s3-file-repository';
+import { DefaultMimetypeExtensionConverter } from './util/default-mimetype-extension-converter';
 
 describe('FileRepositoryModule', () => {
-  it('make disk file service by disk config', async () => {
+  it('make disk file repository by disk config', async () => {
     // given
     const diskConfig: DiskFileUploadConfiguration = {
       strategy: UploadStrategy.DISK,
@@ -24,12 +26,41 @@ describe('FileRepositoryModule', () => {
 
     // when
     const fileService = module.get(FileRepository);
-    const nameGenerator = module.get(NAME_GENERATOR);
     const config = module.get(CONFIG);
 
     // then
     expect(fileService).toBeInstanceOf(DiskFileRepository);
-    expect(nameGenerator).toBeInstanceOf(UuidNameGenerator);
     expect(config).toBe(diskConfig);
+  });
+
+  it('make s3 file repository by s3 config', async () => {
+    // given
+    const s3Config: S3FileUploadConfiguration = {
+      strategy: UploadStrategy.S3,
+      options: {
+        credentials: {
+          accessKeyId: 'test',
+          secretAccessKey: 'test',
+        },
+        acl: 'public-read',
+        bucket: 'test',
+        region: 'test',
+      },
+    };
+    const module = await Test.createTestingModule({
+      imports: [FileRepositoryModule.register(s3Config)],
+    }).compile();
+
+    // when
+    const fileService = module.get(FileRepository);
+    const config = module.get(CONFIG);
+    const mimetypeExtensionConverter = module.get(MIMETYPE_EXTENSION_CONVERTER);
+
+    // then
+    expect(fileService).toBeInstanceOf(S3FileRepository);
+    expect(mimetypeExtensionConverter).toBeInstanceOf(
+      DefaultMimetypeExtensionConverter,
+    );
+    expect(config).toBe(s3Config);
   });
 });
