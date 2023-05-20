c45537fbecf9dd2cdc48f53793e4961f70d1d010
Kim Myeongil <mikim961103@gmail.com>
2023-05-21 01:27:36 +0900

    feat: add test case s3 repository

--- lib/file-repository/s3-file-repository.spec.ts
+++ lib/file-repository/s3-file-repository.spec.ts
@@ -34,6 +34,33 @@ describe('S3FileRepository', () => {
     expect(response).toBe('test-path/file.txt');
   });
 
+  it('upload file in s3 when no path in options of config', async () => {
+    // given
+    const config: S3FileUploadConfiguration = {
+      strategy: UploadStrategy.S3,
+      options: {
+        region: 'test',
+        credentials: {
+          accessKeyId: 'test',
+          secretAccessKey: 'test',
+        },
+        endPoint: 'http://localhost:4566',
+        forcePathStyle: true,
+        bucket: 'test-bucket',
+        acl: 'public-read',
+      },
+    };
+    const s3FileRepository = new S3FileRepository(config);
+
+    const file = new File('file.txt', Buffer.from('hello'));
+
+    // when
+    const response = await s3FileRepository.save(file);
+
+    // then
+    expect(response).toBe('file.txt');
+  });
+
   it('throw timeout exception when it times out', async () => {
     // given
     const config: S3FileUploadConfiguration = {
