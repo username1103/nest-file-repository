21c9ba3d74d27264005e03986da4ac82e911d1cc
Kim Myeongil <67570061+username1103@users.noreply.github.com>
2023-05-27 21:01:46 +0900

    docs: add custom provider document (#12)

--- lib/file-repository/index.ts
+++ lib/file-repository/index.ts
@@ -4,3 +4,4 @@ export * from './constant';
 export * from './interface/file-repository-configuration';
 export * from './interface/s3-upload-option-factory';
 export * from './exception';
+export * from './s3-file-repository/default-s3-upload-option-factory';
